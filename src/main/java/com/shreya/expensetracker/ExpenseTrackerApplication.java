91d46ddc7654741ec4fa94dc1c25b974bf87fb91
Shreya Jha <shreyajha2430@gmail.com>
2026-06-30 23:48:45 +0530

    imp note

--- src/main/java/com/shreya/expensetracker/ExpenseTrackerApplication.java
+++ src/main/java/com/shreya/expensetracker/ExpenseTrackerApplication.java
@@ -10,4 +10,6 @@ public class ExpenseTrackerApplication {
         SpringApplication.run(ExpenseTrackerApplication.class, args);
     }
 
-}
+}//Why should the class annotated with @SpringBootApplication be placed in the root package?
+//Because @SpringBootApplication includes @ComponentScan, which scans the package containing the main application class and all its sub-packages. Placing it in the root package ensures that Spring discovers all controllers, services, repositories, and other components in the application.
+//note down to notes
